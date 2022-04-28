dcfcf79c55ffcd0fa8065121a0620946885de5da
angelikiKS <s212881@student.dtu.dk>
2022-04-28 11:52:25 +0000

    regression curves for LXN, IL8 created

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -1,6 +1,6 @@
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
-
+library("ggplot2")
 
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
@@ -12,7 +12,7 @@ treatment_w_meta <- read_tsv(file = "./data/02_treatment_w_meta_clean.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
-large_mod <- large_w_meta %>% 
+long_model <- large_w_meta %>% 
   select(!c(sex, age, acc_num)) %>% 
   filter(disease == c("Normal","Rheumatoid arthritis (early)")) %>% 
   mutate(outcome = case_when(disease == "Normal" ~ 0,
@@ -26,7 +26,7 @@ test_data<- treatment_w_meta %>%
   
 
 # Model data
-earlyRA.model <- large_mod %>%
+earlyRA.model <- long_model %>%
   glm(outcome ~ LXN + IL8,
       data = .,
       family = binomial(link = "logit"))
@@ -41,12 +41,34 @@ test_data$y_pred = predict(earlyRA.model, test_data, type="response")
 
 
 # Visualise data ----------------------------------------------------------
+LXN_curve <- long_model %>% 
+  ggplot(mapping =  aes(x=LXN, y=outcome)) + 
+  geom_point(alpha=.5) +
+  stat_smooth(method="glm",
+              se=FALSE,
+              method.args = list(family=binomial)
+  )
 
-# relationship of data
+IL8_curve <- long_model %>% 
+  ggplot(mapping =  aes(x=IL8, y=outcome)) + 
+  geom_point(alpha=.5) +
+  stat_smooth(method="glm",
+              se=FALSE,
+              method.args = list(family=binomial)
+  )
 
-my_data_clean_aug %>% ...
+
+LXN_curve
+IL8_curve
+
 
 
 # Write data --------------------------------------------------------------
-write_tsv(...)
-ggsave(...)
+ggsave("model1_LXN_curve.png",
+       LXN_curve,
+       path = "/cloud/project/results")
+
+ggsave("model1_IL8_curve.png",
+       IL8_curve,
+       path = "/cloud/project/results")
+
