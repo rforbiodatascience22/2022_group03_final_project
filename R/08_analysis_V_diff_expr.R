aaf7671e5461cd8e9a273b0bf0c26d8e74f35acd
madscort <madsn@msn.com>
2022-05-07 13:00:58 +0000

    added gene list

--- R/08_analysis_V_diff_expr.R
+++ R/08_analysis_V_diff_expr.R
@@ -11,15 +11,39 @@ all_logfold <- read_tsv(file = "data/03_combined_log2fc.tsv")
 
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean_aug %>% ...
 
+# Find top 100 over and under expressed genes between healthy and pre
+# treatment samples:
 
+# Extract lists of genes:
 
+over_expr <- all_logfold %>% 
+  filter(differentiation == "Healthy vs. Pre treatment",
+         significant) %>% 
+  arrange(desc(log2FoldChange))
+  select(gene) %>% 
+  slice_head(n = 100) %>% 
+  mutate("diff_expr" = "over")
+
+under_expr <- all_logfold %>% 
+  filter(differentiation == "Healthy vs. Pre treatment",
+         significant) %>%
+  arrange(desc(log2FoldChange)) %>%
+  select(gene) %>% 
+  slice_tail(n = 100) %>% 
+  mutate("diff_expr" = "under")
+
+# Combine lists:
+
+diff_expr_genes <- over_expr %>% bind_rows(under_expr)
 
 # Visualise data ----------------------------------------------------------
 my_data_clean_aug %>% ...
 
 
 # Write data --------------------------------------------------------------
-write_tsv(...)
+
+# List of differentially expressed genes:
+write_tsv(x = diff_expr_genes,
+          file = "data/08_diff_expr_genes.tsv")
 ggsave(...)
